dd37d7a70424ea546511802fb084bd6261bb37cb
Muhammedsalihkk <163625741+Muhammedsalihkk@users.noreply.github.com>
2025-03-17 20:10:15 +0530

    Time: 408 ms (5.06%), Space: 59.8 MB (33.8%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+for(i=0;i<nums.length;i++)
+{
+   for(j=i+1;j<nums.length;j++)
+   {
+      if(nums[i]==0)
+      {
+         temp=nums[i]
+         nums[i]=nums[j]
+         nums[j]=temp
+      }
+      
+   }
+}
+};
