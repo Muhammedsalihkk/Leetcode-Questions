50a23674a9e1004615a6e79196a6b46d7466d3cb
Muhammedsalihkk <163625741+Muhammedsalihkk@users.noreply.github.com>
2025-04-25 19:35:41 +0530

    Time: 46 ms (5.85%), Space: 62.6 MB (5.01%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let nums1=[]
+    let nums2=[]
+    for(i=1;i<=n;i++)
+    {
+       if(i%m!=0)
+       {
+        nums1.push(i)
+       }
+       else{
+        nums2.push(i)
+       }
+    }
+    sum1=nums1.reduce((acc,number)=>acc+number,0)
+    sum2=nums2.reduce((acc,number)=>acc+number,0)
+    console.log(nums1)
+    console.log(nums2)
+    return sum1-sum2
+};
